7b0e21a87a422477393ccbf54b61556411051e43
Dina <sd@myexe.net>
2025-11-17 17:30:30 +0300

    Delete imports

--- src/test/java/screens/wiki/WikiMainScreen.java
+++ src/test/java/screens/wiki/WikiMainScreen.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.AppiumBy;
 import io.qameta.allure.Step;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
