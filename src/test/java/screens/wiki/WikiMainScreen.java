48b9118879c33f0725e60d637ee0791f6afb13fe
Dina <sd@myexe.net>
2025-12-21 12:15:53 +0300

    Update imports

--- src/test/java/screens/wiki/WikiMainScreen.java
+++ src/test/java/screens/wiki/WikiMainScreen.java
@@ -4,10 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.AppiumBy;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.androidUIAutomator;
 
 public class WikiMainScreen {
 
